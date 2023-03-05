532a6af509122af65604f14271c81c75e9e7e424
Thadbeera Farhath <tfarhath665@gmail.com>
2023-03-05 16:09:31 +0530

    added security group

--- main.tf
+++ main.tf
@@ -9,7 +9,28 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "ssh request"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
 resource "aws_instance" "fast_api" {
   instance_type = "t2.micro"
   ami = "ami-0557a15b87f6559cf"
+  securitysecurity_groups = [aws_security_group.allow_ssh.name] 
 }
